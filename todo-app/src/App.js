9176a9fe8882d8f8853b9c2d4fabae7c5570cb9e
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-08-21 17:30:30 +0430

    npm id from uuid

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -11,20 +11,20 @@ const App = () => {
   // const [todoTitle,setTodoTitle]=useState("");
   // const [todoText, setTodoText] = useState("");
 const [todoList, setTodoList] = useState([
-  { title: "job1", text: "salam01" },
+  { title: "job1", text: "salam01" , id:0 },
   
 ]);
-
+console.log(todoList);
 
   return (
     <article className="bg-todo-main">
       <section className="container">
         <section className="todo-main row align-items-center">
           <section className="col-md-6 h-100">
-            <TypeTodo  setTodoList={setTodoList} todoList={todoList} />
+            <TypeTodo  setTodoList={setTodoList} todoList={todoList}  />
           </section>
           <section className="col-md-6 h-100">
-            {todoList.map(item=>(<Todo title={item.title} text={item.text}
+            {todoList.map(item=>(<Todo title={item.title} text={item.text} key={item.id}
 
             /*todoText={todoText} todoTitle={todoTitle} */
             />))}
--- todo-app/src/components/Type-todo.jsx
+++ todo-app/src/components/Type-todo.jsx
@@ -1,22 +1,26 @@
 
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
 const TypeTodo = ({ setTodoList, todoList }) => {
-  const [newTodo,setNewTodo]=useState({title:"",text:""})
+  const [newTodo,setNewTodo]=useState({title:"",text:"" ,id:0})
   return (
     <article className="type-todo-section d-flex flex-column justify-content-around  h-100">
       <h2>To do List</h2>
       <input
         type="text"
         placeholder="Type Your Title"
-        onChange={(e) => setNewTodo({...newTodo,title:e.target.value})}
+        onChange={(e) =>
+          setNewTodo({ ...newTodo, title: e.target.value, id: uuidv4() })
+        }
       />
       <textarea
-        placeholder="Start Typing Here..." onChange={(e)=> setNewTodo({...newTodo,text:e.target.value})}
+        placeholder="Start Typing Here..."
+        onChange={(e) => setNewTodo({ ...newTodo, text: e.target.value })}
       ></textarea>
       <button
         className="btn todo-btn"
-        onClick={(e) =>setTodoList([...todoList,newTodo])}
+        onClick={(e) => setTodoList([...todoList, newTodo])}
       >
         Submit Todo
       </button>
